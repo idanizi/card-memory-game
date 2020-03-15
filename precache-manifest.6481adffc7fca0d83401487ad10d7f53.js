9da0bb41c48a57ae60f2da629e23aa75c44c3950
Idan Izicovich <idanizi@gmail.com>
2020-03-15 03:20:29 +0200

    Updates

---
+++ precache-manifest.6481adffc7fca0d83401487ad10d7f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378970f69aac5b9df31a55cd6eb079c5",
+    "url": "/$%7Bnpm_package_name%7D/index.html"
+  },
+  {
+    "revision": "8397ac0efdddf99b4386",
+    "url": "/$%7Bnpm_package_name%7D/static/css/main.0aa0e6ac.chunk.css"
+  },
+  {
+    "revision": "f215ea7846d4fa17ce1c",
+    "url": "/$%7Bnpm_package_name%7D/static/js/2.bc94ff58.chunk.js"
+  },
+  {
+    "revision": "c4643dd0ebb7319e096368d0f8ffca53",
+    "url": "/$%7Bnpm_package_name%7D/static/js/2.bc94ff58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8397ac0efdddf99b4386",
+    "url": "/$%7Bnpm_package_name%7D/static/js/main.372c3f3a.chunk.js"
+  },
+  {
+    "revision": "eb5a9062bc9fda22dc1f",
+    "url": "/$%7Bnpm_package_name%7D/static/js/runtime-main.cf8ba41c.js"
+  }
+]);

b33883e8f40aaa1382954c4f815b2288e9f37fdc
Idan Izicovich <idanizi@gmail.com>
2020-04-11 01:34:10 +0300

    index fix

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const proxyProt = 8000;
 const app = express();
 
 app.use(['/api/*', '/socket*'], createProxyMiddleware({ target: `http://localhost:${proxyProt}`, changeOrigin: true }));
-app.use('/*', express.static(path.resolve('client/build')))
+
+app.use(express.static(path.resolve('client', 'build')))
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'))
+})
 app.listen(port, () => {
     console.log(`serving on ${port}, proxy to ${proxyProt}`)
 });

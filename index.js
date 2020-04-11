817cd24d66c9dcb9acc03560e07c8a52c12e489e
Idan Izicovich <idanizi@gmail.com>
2020-04-11 15:34:17 +0300

    get "/"

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(['/api/*', '/socket*'], createProxyMiddleware({ target: `http://localhos
 
 app.use(express.static(path.join(__dirname, 'client', 'build')))
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
 

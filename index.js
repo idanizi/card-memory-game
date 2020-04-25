c44a8c2ce91d658f542a12c37d8f46834bdde48c
Idan Izicovich <idanizi@gmail.com>
2020-04-25 23:55:05 +0300

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path')
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const clientNpm = require('./client/package.json')
+const serverNpm = require('./server/package.json')
 
 const port = process.env.PORT || 8080;
 const proxyProt = 8000;
@@ -8,6 +10,10 @@ const proxyProt = 8000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+app.get('/version', (req, res) => {
+    res.json({client: clientNpm.version, server: serverNpm.version})
+})
 
 app.use(['/api/*', '/socket*'], createProxyMiddleware({ target: `http://localhost:${proxyProt}`, changeOrigin: true }));
 
